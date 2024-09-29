41e94fd6f0a45607c36f5feb19bc29de82080246
Arman <armandwi.pangestu7@gmail.com>
2024-09-29 21:46:57 +0700

    database: setup category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
             ProfileSeeder::class,
             RajaOngkirProvinceSeeder::class,
             AddressSeeder::class,
+            CategorySeeder::class,
         ]);
     }
 }
