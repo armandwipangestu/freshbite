6713334fc4d6cc7a79db0f7c2748e415f1f4cfc2
Arman <armandwi.pangestu7@gmail.com>
2024-09-30 13:31:20 +0700

    cms: add default user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
@@ -15,6 +16,13 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        User::create([
+            'name' => 'Admin',
+            'avatar' => fake()->imageUrl(),
+            'email' => 'admin@freshbite.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+        ]);
         User::factory(10)->create();
     }
 }
