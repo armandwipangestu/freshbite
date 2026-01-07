b0b70a0c69ea380978427be87e5a77f17b931610
armandwipangestu <armandwi.pangestu7@gmail.com>
2026-01-07 10:32:09 +0700

    fix: mismatch content HTTP Header & force HTTPS for production mode

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
 
 class TrustProxies extends Middleware
 {
@@ -12,4 +13,9 @@ class TrustProxies extends Middleware
      * @var array<int, string>|string|null
      */
     protected $proxies = '*';
+
+    protected $headers = Request::HEADER_X_FORWARDED_FOR
+    | Request::HEADER_X_FORWARDED_HOST
+    | Request::HEADER_X_FORWARDED_PORT
+    | Request::HEADER_X_FORWARDED_PROTO;
 }
