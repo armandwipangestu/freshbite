7ef90753d4506e74a54b6073facbf08125dff093
armandwipangestu <armandwi.pangestu7@gmail.com>
2026-01-11 02:12:12 +0700

    fix: add force HTTPS even if APP_ENV local

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('production')) {
+        if (app()->environment('production') || str_starts_with(config('app.url') ?? '', 'https://')) {
             URL::forceScheme('https');
         }
 
