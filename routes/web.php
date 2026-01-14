410200526c62837964e36e93c66ebb88057880d0
armandwipangestu <armandwi.pangestu7@gmail.com>
2026-01-14 16:13:09 +0700

    chore: disable dashboard route

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return Inertia::render('Dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
